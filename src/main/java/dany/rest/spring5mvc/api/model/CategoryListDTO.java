f87faf6ab0cda57dd712ed5ccff6899f321be452
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-04-04 16:51:34 -0500

    Controll Categories and Test

---
+++ src/main/java/dany/rest/spring5mvc/api/model/CategoryListDTO.java
@@ -0,0 +1,15 @@
+package dany.rest.spring5mvc.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Created by bautisj on 4/4/2018.
+ */
+@Data
+@AllArgsConstructor
+public class CategoryListDTO {
+    List<CategoryDTO> categoryDTOS;
+}
---
+++ src/main/java/dany/rest/spring5mvc/controllers/v1/CategoryController.java
@@ -0,0 +1,40 @@
+package dany.rest.spring5mvc.controllers.v1;
+
+import dany.rest.spring5mvc.api.model.CategoryDTO;
+import dany.rest.spring5mvc.api.model.CategoryListDTO;
+import dany.rest.spring5mvc.services.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.http.HttpStatus.OK;
+
+/**
+ * Created by bautisj on 4/4/2018.
+ */
+@Controller
+@RequestMapping("/api/v1/categories/")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<CategoryListDTO> getallCategories(){
+        return new ResponseEntity<CategoryListDTO>(
+                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    }
+
+    @GetMapping("{name}")
+    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name){
+        return new ResponseEntity<CategoryDTO>(
+                categoryService.getCategoryByName(name), HttpStatus.OK
+        );
+    }
+}
